aeaf8a752e60fa153c0c4194e87546792f6d7639
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-06-01 00:49:44 +0100

    added background image

---
+++ portofolio/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id="main">
+        <img className='w-full h-screen object-cover object-left ' src='https://i.imgur.com/OEpPsEd.jpg'></img>
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/20'>
+         <div>
+            <h1>I'm Alen</h1>
+            <h2>I'm a</h2>
+         </div>   
+        </div>
+    </div>
+  )
+}
+
+export default Main
