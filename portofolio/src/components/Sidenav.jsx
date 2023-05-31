05370e8e2d2a9e622eb7775ad0a6df92213335df
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-05-31 01:33:52 +0100

    adding sidenav

--- portofolio/src/components/Sidenav.jsx
+++ portofolio/src/components/Sidenav.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import {AiOutlineMenu} from 'react-icons/ai'
 const Sidenav = () => {
   return (
-    <div>Sidenav</div>
+    <div>
+        <AiOutlineMenu />
+    </div>
   )
 }
 
