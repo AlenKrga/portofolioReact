2d8ac76da4529f1ffa352dd5e0a0533fe58e9c17
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-06-02 23:46:12 +0100

    adding components work

---
+++ portofolio/src/components/Work.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Workitem from './Workitem'
+const data = [
+    { 
+        year: 2020,
+        title: 'Full stack developer',
+        duration: '2 Years',
+        details: 'lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        year: 2018,
+        title: 'Front End Developer',
+        duration: '2 Years',
+        details: 'lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+    },
+    
+]
+const Work = () => {
+  return (
+    <div>Work</div>
+  )
+}
+
+export default Work
