5fca220cb62a1911b2289212ee4e329af903d1f1
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-06-05 23:47:46 +0100

    updating worksitem style

--- portofolio/src/components/Workitem.jsx
+++ portofolio/src/components/Workitem.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Workitem = ({year, title, duration, details}) => {
   return (
     <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
-      <li>
-        <div />
-        <p>
-            <span>{year}</span>
-            <span>{title}</span>
-            <span>{duration}</span>
+      <li className="mb-10 ml-4">
+        <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white" />
+        <p className="flex flex-wrap gap-4 flex-row items-center jusstify-start text-xs md:text-sm">
+            <span className="inline-block px-2 py-1 font-semibold text-white bg-cyan-600 rounded-md">{year}</span>
+            <span className="text-lg font-semibold text-cyan-600">{title}</span>
+            <span className="my-1 text-sm font-normal leading-none text-stone-400">{duration}</span>
             
         </p>
-        <p>
+        <p className="my-2">
             {details}
         </p>
       </li>
